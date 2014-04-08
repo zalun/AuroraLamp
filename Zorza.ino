befa00f961e200140d828d44eefdd1b61a7bd7fb
Piotr Zalewa <piotr@zalewa.info>
2014-04-08 23:23:51 +0200

    refactored, more fluent light changes

--- Zorza.ino
+++ Zorza.ino
@@ -1,35 +1,39 @@
 int LED[] ={9, 10, 11, 3, 5, 6};  // RGB, RGB
-const int LIGHTPIN = 0;
+const int AMBIENTPIN = 0; // measuring ambient light on pin A0
 const int VOLTAGE = 7; // Providing +5V to the pin
-const int PINCOUNT = 6;
-float lightness[] = {1, 1, 1, 1, 1, 1};
-float steps[] = {0.7, 0.8, 0.9, 0.75, 0.85, 0.75};
-int lightValue;
 
-const int STEP = 10; // milliseconds to change the values
-const int PI180 = PI/180;
+const int PINCOUNT = 6;
+float degree[] = {1, 1, 1, 1, 1, 1};
+float steps[] = {0.17, 0.19, 0.23, 0.21, 0.18, 0.16}; // changes of the degrees for each pin
+
+int ambientLight;
+int lightToPin;
+
+const int STEP = 5; // milliseconds to change the values
+const float PI180 = 0.01745329251;
 const int BRIGHT = 10; // switch on if darker 
 
 void setup() {
    Serial.begin(9600);
    for (int i = 0; i < PINCOUNT; i++) {
      pinMode(LED[i], OUTPUT);
-     lightness[i] = random(255);
+     degree[i] = random(360); // randomize starting colors
    }
    pinMode(VOLTAGE, OUTPUT);
 }
 
 void loop()
 {
-   lightValue = analogRead(LIGHTPIN);
-   if (lightValue < BRIGHT) {
+   ambientLight = analogRead(AMBIENTPIN);
+   if (ambientLight < BRIGHT) {
      digitalWrite(VOLTAGE, HIGH);
      for (int i = 0; i < PINCOUNT; i++) {
-       if (lightness[i] >= 255 or lightness[i] <= 0) {
-         steps[i] = -1 * steps[i];
+       if (degree[i] >= 360) {
+         degree[i] = 0;
        }
-       lightness[i] = lightness[i] + steps[i];
-       analogWrite(LED[i], (int)lightness[i]);
+       degree[i] = degree[i] + steps[i];
+       lightToPin = (1 + sin(degree[i] * PI180)) * 127;
+       analogWrite(LED[i], (int)lightToPin);
      }
    } else {
      digitalWrite(VOLTAGE, LOW);
